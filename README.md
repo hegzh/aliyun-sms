9013e521de32ff85d5adbe23e1887af7352a63aa
Gilbert <heguangzong@spek.hk>
2017-07-21 13:58:13 +0800

    fix bugs

--- README.md
+++ README.md
@@ -2,13 +2,12 @@
 
 阿里云短信服务 PHP SDK 的 composer 封装
 
-TP5 thinkphp5
+TP5
 
 [帮助文档](https://help.aliyun.com/document_detail/55451.html?spm=5176.sms-account.109.2.56907c16xG4lWM)
 
 
-## TP5 config
-
+## Thinkphp 5 config 配置信息
 ```
 $config['alidayu'] = [
         'app_key' => 'LTAIRdKEkYqxxxxx',
@@ -18,22 +17,46 @@ $config['alidayu'] = [
 ```
 
 ## Examples
-
 ```
 <?php
 use hegzh\AliyunSms\AliyunSms;
 
 $mobile = "13800138000"; //国内手机号码
 $templateCode = "SMS_75990000"; //短信模板 阿里云上申请
-$templateParm = ['customer' => '客户名称'];//短信模板变量参数
-$smsId = 1111; //短信业务流水id
+$templateParms = ['customer' => 'hegzh'];//短信模板变量参数
+$bizId = 1111; //短信业务流水id
 
 $sms = new AliyunSms();
 //发送短信
-$res = $sms->sendSms($mobile, $templateCode, $templateParm, $smsId);
+$res = $sms->sendSms($mobile, $templateCode, $templateParms, $bizId);
+Response:
+{
+    "state": false, //发送状态
+    "code": "InvalidAccessKeyId.NotFound", //错误编码
+    "error": "Specified access key is not found.", //错误消息
+}
 
 //查询短信记录
 $date = "20170720";
-$res = $sms->getSendDetails($mobile, $date);
+$res = $sms->getSmsDetails($mobile, $date);
+Response:
+{
+    "state": true, //返回状态
+    "total": 2, //总记录数
+    'code' => 'InvalidAccessKeyId.NotFound', //错误编码
+    'error' => 'Specified access key is not found.', //错误消息
+    "data": [
+        {
+            "OutId": "277770", //外部流水id
+            "SendDate": "2017-07-21 11:11:25", //发送情趣时间
+            "SendStatus": 3, //发送状态
+            "ReceiveDate": "2017-07-21 11:11:25", //接收时间
+            "ErrCode": "DELIVRD", //错误代码
+            "TemplateCode": "SMS_75990400", // 短信模板编码
+            "Content": "【签名】尊敬的hegzh，欢迎您使用阿里大鱼短信服务，阿里大鱼将为您提供便捷的通信服务！",
+            "PhoneNum": "13800138000", //手机号码
+        }
+    ]
+}
 ?>
 ```
--- src/AliyunSms.php
+++ src/AliyunSms.php
@@ -9,6 +9,8 @@ namespace hegzh\AliyunSms;
  * Date: 2017/7/18
  * Time: 15:34
  */
+require_once VENDOR_PATH . "/hegzh/aliyun-core/src/Config.php";
+
 use hegzh\AliyunCore\Profile\DefaultProfile;
 use hegzh\AliyunCore\DefaultAcsClient;
 use hegzh\AliyunSms\Request\V20170525\SendSmsRequest;
@@ -38,12 +40,11 @@ class AliyunSms
      * 发送短信
      * @param string $mobile 手机号码 136128xxxxx
      * @param string $templateCode 短信
-     * @param array $templateParm 短信模板 ['customer' => '阿里大于']
-     * @param int $smsId 短信id
+     * @param array $templateParms 短信模板 ['customer' => '阿里大于']
+     * @param int $bizId 外部流水id
      */
-    public function sendSms($mobile, $templateCode, $templateParm = [], $smsId = 0)
+    public function sendSms($mobile, $templateCode, $templateParms = [], $bizId = '')
     {
-        $smsId = empty($smsId) ? mt_rand(100000, 999999) : (int)$smsId;
         //初始化访问的acsCleint
         $profile = DefaultProfile::getProfile($this->region, $this->accessKeyId, $this->accessKeySecret);
         DefaultProfile::addEndpoint($this->region, $this->region, $this->product, $this->domain);
@@ -53,12 +54,14 @@ class AliyunSms
         $request->setPhoneNumbers($mobile); //必填-短信接收号码
         $request->setSignName($this->accessSignName);//必填-短信签名
         $request->setTemplateCode($templateCode);//必填-短信模板Code
-        $request->setTemplateParam(json_encode($templateParm));//选填-假如模板中存在变量需要替换则为必填(JSON格式)
-        $request->setOutId($smsId);//选填-发送短信流水号
+        $request->setTemplateParam(json_encode($templateParms));//选填-假如模板中存在变量需要替换则为必填(JSON格式)
+        if (!empty($bizId)) {
+            $request->setOutId($bizId);//选填-发送短信流水号
+        }
 
-        //发起访问请求
         $return = ['state' => true];
         try {
+            //发起访问请求
             $acsResponse = $acsClient->getAcsResponse($request);
             if ($acsResponse->Code != 'OK') {
                 $return['state'] = false;
@@ -84,29 +87,60 @@ class AliyunSms
      * @param string $date 格式yyyyMMdd 20170720
      * @param int $page 当前页码
      * @param int $pageSize 每页长度
-     * @param string $smsId 短信id
+     * @param string $bizId 外部流水id
      */
-    public function getSendDetails($mobile, $date, $page = 1, $pageSize = 10, $smsId = '')
+    public function getSmsDetails($mobile, $date, $page = 1, $pageSize = 10, $bizId = '')
     {
         //初始化访问的acsCleint
         $profile = DefaultProfile::getProfile($this->region, $this->accessKeyId, $this->accessKeySecret);
         DefaultProfile::addEndpoint($this->region, $this->region, $this->product, $this->domain);
         $acsClient = new DefaultAcsClient($profile);
-
         $request = new QuerySendDetailsRequest();
 
         $request->setPhoneNumber($mobile);//必填-短信接收号码
-        if (!empty($smsId)) {
-            $request->setBizId($smsId);//选填-短信发送流水号
+        if (!empty($bizId)) {
+            $request->setBizId($bizId);//选填-短信发送流水号
         }
-
         $request->setSendDate($date);//必填-短信发送日期，支持近30天记录查询，格式yyyyMMdd
         $request->setPageSize($pageSize);//必填-分页大小
-        $request->setContent($page);//必填-当前页码
+        $request->setCurrentPage($page);//必填-当前页码
 
-        //发起访问请求
-        $acsResponse = $acsClient->getAcsResponse($request);
-        return $acsResponse;
+        $return = ['state' => true];
+        try {
+            //发起访问请求
+            $acsResponse = $acsClient->getAcsResponse($request);
+            if ($acsResponse->Code != 'OK') {
+                $return['state'] = false;
+                $return['code'] = $acsResponse->Code;
+                $return['error'] = $acsResponse->Message;
+            }
+            $return['total'] = $acsResponse->TotalCount; //总数
+            if (!empty($acsResponse->SmsSendDetailDTOs->SmsSendDetailDTO)) {
+                $tmp = $acsResponse->SmsSendDetailDTOs->SmsSendDetailDTO;
+                foreach ($tmp as $v) {
+                    $return['data'][] = [
+                        'OutId' => $v->OutId, //流水id
+                        'SendDate' => $v->SendDate, //发送日期
+                        'SendStatus' => $v->SendStatus, //发送状态
+                        'ReceiveDate' => $v->ReceiveDate, //接收状态
+                        'ErrCode' => $v->ErrCode, //错误代码
+                        'TemplateCode' => $v->TemplateCode, //短信编号
+                        'Content' => $v->Content,
+                        'PhoneNum' => $v->PhoneNum,
+                    ];
+                }
+            }
+        } catch (ClientException  $e) {
+            $return['state'] = false;
+            $return['code'] = $e->getErrorCode();
+            $return['error'] = $e->getErrorMessage();
+        } catch (ServerException  $e) {
+            $return['state'] = false;
+            $return['code'] = $e->getErrorCode();
+            $return['error'] = $e->getErrorMessage();
+        }
+
+        return $return;
     }
 
 }
